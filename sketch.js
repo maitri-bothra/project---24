6b2170cb2acfe5154023b8441c643e7114a07f3a
maitri-bothra <73102658+maitri-bothra@users.noreply.github.com>
2020-10-31 15:48:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var Paper
+
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	Paper = new Paper(100,100);
+	
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+
+
+
+
+  Paper.display();
+ 
+}
+
+
+
